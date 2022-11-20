a75006607032cab8964fe0d7ef5a411b2038bbc4
selmankon <12685802+selmankon@users.noreply.github.com>
2022-11-20 13:33:07 +0300

    optimization with curl, requests removed

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
-from utils import control_target, manage_target, is_404
+from get_links import get_links
 
-pages = []
 TARGET_URL = "http://localhost:5500"
 
-def get_links(url):
-    global pages
-
-    html = requests.get(f'{url}').text
-
-    soup = BeautifulSoup(html, "html.parser")
-    for link in soup.find_all("a"):
-        if "href" in link.attrs:
-            potential_page = manage_target(TARGET_URL, link.attrs["href"])
-            if (potential_page not in pages) and (control_target(TARGET_URL, potential_page)) and (not is_404(potential_page)):
-                print(url, " ~~> ", potential_page)
-                pages.append(potential_page)
-                get_links(potential_page)
-
-
 def main():
-    get_links(TARGET_URL)
-    #print("\n".join(pages))
-
+    get_links(TARGET_URL, TARGET_URL)
 
 if __name__ == "__main__":
     try:
