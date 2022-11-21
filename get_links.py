d062d0ce4f94db3c876a172985f2321ab173bf50
Zekeriya AY <zekeriya@zekeriyaay.com>
2022-11-21 23:15:51 +0300

    Refactored get_links

--- get_links.py
+++ get_links.py
@@ -1,4 +1,4 @@
-from utils import control_target, manage_target, cmd
+from utils import control_target, manage_target, cmd, control_status_code
 from bs4 import BeautifulSoup
 
 pages = []
@@ -7,24 +7,27 @@ pages = []
 def get_links(TARGET_URL, url):
     global pages
 
+    potential_pages = []
+
     html = cmd(f'curl -Ls {url}')
     soup = BeautifulSoup(html, "html.parser")
 
+    # Get all potential links
     for link in soup.find_all("a"):
         if "href" in link.attrs:
             # variable name is potential_pages because of
             # there is a potential page and potential page may a redirect page
-            potential_pages = manage_target(TARGET_URL, link.attrs["href"])
+            potential_page = manage_target(TARGET_URL, link.attrs["href"])
+            if control_target(TARGET_URL, potential_page):
+                potential_pages.append(potential_page)
 
-            # YUKARIDAKİ FOR İLE SAYFADAKİ TÜM LİNKLERİ CRAW EDİLECEK, STATUS_CODE KONTROLÜ VEYA RECURSIVE OLMAYACAK
-            # AŞAĞIDA DA PAGES İÇERİSİNDEKİLERİ KONTROL EDİP RECURSİVE OLARAK TEKRAR TARANACAK
-            # BUNUN İÇİN 2. BİR PAGES LİSTESİ YAPILABİLİR GİBİ
+    # Control potential pages
+    for potential_page in potential_pages:
+        if potential_page not in pages:
+            page = control_status_code(potential_page)
+            if page:
+                print(*page, sep="\n")
+                for p in page:
+                    pages.append(p)
 
-            if potential_pages not in pages:
-                potential_pages = control_target(TARGET_URL, potential_pages)
-                if potential_pages:
-                    print(url, " ~~> ", potential_pages)
-                    for potential_page in potential_pages:
-                        pages.append(potential_page)
-
-                    get_links(TARGET_URL, potential_pages[0])  # just get that finish with / one ~> get blog/, not blog
+                get_links(TARGET_URL, page[0])
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from get_links import get_links
 
 TARGET_URL = "http://localhost:5500"
 
+
 def main():
     get_links(TARGET_URL, TARGET_URL)
+
 
 if __name__ == "__main__":
     try:
